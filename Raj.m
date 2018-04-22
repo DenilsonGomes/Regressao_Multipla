df6ea5e64f95d37b9e9f1fb96c3a0e80de1aed2e
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-04-22 14:13:41 -0300

    Criação da funcao Raj

---
+++ Raj.m
@@ -0,0 +1,30 @@
+%Autor: Denilson Gomes Vaz da Silva
+%Graduando em Engenharia da Computação
+%Inteligência Computacional - Dr. Jarbas Joaci
+%Função que retorna o Coeficiente de Determinação Ajustado R^2aj
+
+%função recebe (valores reais, valores estimados, matriz dos polinomios) e 
+%retorna o coeficiente de determinação R^2 ajuastado
+function coef = Raj(Y,Yout,poli)
+
+[~,grau] = size(poli); %grau do polinimio
+[tamanho,~] = size(Y); %tamanho do vetor de saidas
+
+ymed = sum(Y)/tamanho;
+
+SQe = 0;
+for k = 1:tamanho
+   SQe = SQe + (Y(k) - Yout(k))^2; %soma para calcular a somatorio dos
+   %quadrados das diferenças de y e yout
+end   
+%num vale a soma dos quadrados das diferenças entre y e yout
+
+Syy=0;
+for w = 1:tamanho
+   Syy = Syy + (Y(w) - ymed)^2; %soma para calcular a soma dos quadrados 
+   %das diferenças entre y e ymed
+end   
+%den vale a soma dos quadrados das diferenças entre y e ymed
+
+coef =  1 - (SQe/(tamanho - grau + 1))/(Syy/(tamanho - 1)); %coef vale o coeficiente
+%de determinação ajustado
